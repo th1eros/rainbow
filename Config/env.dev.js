2fb07fed74ca7e02538e1f910d54886f9a545ac6
Th1eros <248079803+th1eros@users.noreply.github.com>
2026-06-17 20:02:19 -0300

    chore: remove docker e env.dev locais, adiciona rainbow.json

--- Config/env.dev.js
+++
@@ -1,9 +0,0 @@
-const host = window.location.hostname;
-const isDev = host === 'localhost' || host === '127.0.0.1';
-
-window.__ENV__ = isDev ? 'development' : 'production';
-window.__API_BLUE__ = isDev ? 'http://localhost:5073' : `https://blue.${host.replace(/^(blue|red|violet|silver)\./, '')}`;
-window.__API_RED__ = isDev ? 'http://localhost:5074' : `https://red.${host.replace(/^(blue|red|violet|silver)\./, '')}`;
-window.__API_VIOLET__ = isDev ? 'http://localhost:5075' : `https://violet.${host.replace(/^(blue|red|violet|silver)\./, '')}`;
-window.__API_SILVER__ = isDev ? 'http://localhost:5076' : `https://silver.${host.replace(/^(blue|red|violet|silver)\./, '')}`;
-window.__DEBUG__ = isDev;
